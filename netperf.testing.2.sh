247e579a93627b7b5f295aab25b321238e7dac38
rollrat <rollrat.cse@gmail.com>
2022-07-26 23:34:36 +0900

    Update netperf.testing.2.sh

--- netperf.testing.2.sh
+++ netperf.testing.2.sh
@@ -1,6 +1,8 @@
+echo 0. demigration
 ssh RDMA-09 -t 'bash -l -c "./usernet-module/virsh-migrate.sh usernet-vm4 RDMA-10"'
 
 # start netserver
+echo 1. start netserver
 ssh usernet-vm3 "
 sudo killall netserver
 netserver -p 8864
@@ -9,12 +11,15 @@ netserver -p 8864
 sleep 1
 
 # start netperf
+echo 2. start netperf
 ssh usernet-vm4 "netperf -H 172.16.1.103 -p 8864 -D 1 -l 100 -P 0 > netperf.result.txt" &
 
 # sleep 50
+echo 3. sleep 50s
 sleep 50
 
 # migration
+echo 4. migration
 ssh RDMA-10 -t 'bash -l -c "./usernet-module/virsh-migrate.sh usernet-vm4 RDMA-09"'
 
 # attach
@@ -28,6 +33,7 @@ ssh RDMA-10 -t 'bash -l -c "./usernet-module/virsh-migrate.sh usernet-vm4 RDMA-0
 # ssh usernet-vm4 "sudo ./usernet-module/load-amd-driver.sh"
 
 # sleep 50
+echo 5. sleep 50s
 sleep 50
 
 # recovery
